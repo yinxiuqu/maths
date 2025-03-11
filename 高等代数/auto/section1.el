2c78f56367bba335d8caaed05c081b3db7241761
yinxiuqu <yinxiuqu@debian-BULLSEYE-live-builder-AMD64>
2025-03-11 22:52:32 +0800

    更新完9章6节

--- 高等代数/auto/section1.el
+++ 高等代数/auto/section1.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; -*-
+
 (TeX-add-style-hook
  "section1"
  (lambda ()
---
+++ 高等代数/sections/auto/section9.el
@@ -0,0 +1,39 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "section9"
+ (lambda ()
+   (LaTeX-add-labels
+    "dfn:concept1"
+    "dfn:concept2"
+    "exl:concept1"
+    "exl:concept2"
+    "exl:concept3"
+    "exl:concept4"
+    "fig:projection"
+    "eq:gram-schmidt"
+    "thr:gram-matrix"
+    "thm:accompany"
+    "qus:accompany"
+    "eq:isomorphism1"
+    "eq:isomorphism2"
+    "eq:isomorphism3"
+    "eq:isomorphism4"
+    "thm:isomorphism"
+    "eq:isomorphism5"
+    "eq:isomorphism6"
+    "thm:orthogonal2"
+    "eq:isomorphism7"
+    "eq:isomorphism8"
+    "thy:Self-accompanied1"
+    "ddn:Self-accompanied1"
+    "ddn:Self-accompanied2"
+    "ddn:Self-accompanied3"
+    "thm:Self-accompanied1"
+    "thm:Self-accompanied2"
+    "ddn:Self-accompanied4"
+    "eq:Self-accompanied1"
+    "eq:Complex-normal-operator1"
+    "eq:Complex-normal-operator2"))
+ :latex)
+
